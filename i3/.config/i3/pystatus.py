048726aa84e4410214a139ab8615b86ae70d547e
Josip Janzic <josip@jjanzic.com>
2017-04-27 19:05:31 +0200

    Updated status bar

--- i3/.config/i3/pystatus.py
+++ i3/.config/i3/pystatus.py
@@ -1,12 +1,15 @@
 from i3pystatus import Status
+from i3pystatus.mail.maildir import MaildirMail
+from i3pystatus.updates import pacman
 
 status = Status()
 
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
 #                          ^-- calendar week
-status.register("clock",
-                format="%a %-d %b %H:%M",)
+status.register(
+    "clock",
+    format="%a %-d %b %H:%M", )
 
 # Shows the average load of the last minute and the last 5 minutes
 # (the default value for format is used)
@@ -26,24 +29,30 @@ status.register("uptime")
 
 # This would look like this:
 # Discharging 6h:51m
-status.register("battery",
-                battery_ident='BAT1',
-                format="{status}{percentage:.2f}%",
-                alert=False,
-                status={
-                    "DIS": "↓",
-                    "CHR": "↑",
-                    "FULL": "",
-                },)
-status.register("battery",
-                battery_ident='BAT0',
-                format="{status}{percentage:.2f}%",
-                alert=False,
-                status={
-                    "DIS": "↓",
-                    "CHR": "↑",
-                    "FULL": "",
-                },)
+status.register(
+    "battery",
+    battery_ident='BAT1',
+    format="{status}{percentage:.0f}%",
+    alert=True,
+    full_color='#ffffff',
+    charging_color='#ffffff',
+    status={
+        "DIS": "<",
+        "CHR": ">",
+        "FULL": "",
+    }, )
+status.register(
+    "battery",
+    battery_ident='BAT0',
+    format="{status}{percentage:.0f}%",
+    full_color='#ffffff',
+    charging_color='#ffffff',
+    alert=True,
+    status={
+        "DIS": "<",
+        "CHR": ">",
+        "FULL": "",
+    }, )
 # Shows the address and up/down state of eth0. If it is up the address is shown in
 # green (the default value of color_up) and the CIDR-address is shown
 # (i.e. 10.10.10.42/24).
@@ -57,32 +66,63 @@ status.register("battery",
 #                 format_up="{v4cidr}",)
 
 # Note: requires both netifaces and basiciw (for essid and quality)
-status.register("network",
-                interface="wlan0",
-                format_up="{essid} {v4}",)
+status.register(
+    "network",
+    interface="wlan0",
+    dynamic_color=False,
+    color_up="#ffffff",
+    color_down="#ffffff",
+    format_up="{essid} {v4}", )
 
 # Shows disk usage of /
 # Format:
 # 42/128G [86G]
-status.register("disk",
-                path="/",
-                format="Free: {avail}G",)
+status.register(
+    "disk",
+    path="/",
+    format="{avail}G", )
 
 # Shows pulseaudio default sink volume
 #
 # Note: requires libpulseaudio from PyPI
-status.register("pulseaudio",
-                format="Vol: {volume}",)
+status.register(
+    "pulseaudio",
+    format="Vol: {volume}", )
 
 # Shows mpd status
 # Format:
 # Cloud connected▶Reroute to Remain
-status.register("mpd",
-                format="{status} {title}",
-                status={
-                    "pause": "Paused:",
-                    "play": "Playing:",
-                    "stop": "Stopped",
-                },)
+status.register(
+    "mpd",
+    format="{title} {status}",
+    status={
+        "pause": "-",
+        "play": ">",
+        "stop": "-",
+    }, )
+
+for maildir in ['jjanzic', 'personal', 'insided']:
+    status.register(
+        "mail",
+        backends=[
+            MaildirMail(
+                account=maildir,
+                directory="/home/josip/.mail/{maildir}/inbox".format(
+                    maildir=maildir))
+        ],
+        format="{account} {current_unread}",
+        format_plural="{account} {current_unread}",
+        color_unread="#00ff00")
+
+status.register("external_ip")
+status.register("bitcoin", currency='EUR')
+# status.register("pomodoro",
+#                 sound="~/dotfiles/message.wav")
+
+# status.register(
+#     "updates",
+#     format="Updates: {count}",
+#     format_no_updates="No updates",
+#     backends=[pacman.Pacman()])
 
 status.run()
