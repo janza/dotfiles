3fb17b23d067094a3462652638a01acbf6c55c29
Josip Janzic <josip@jjanzic.com>
2017-06-28 23:20:37 +0200

    Update zsh keybindings

--- zsh/.zshrc.d/keybindings.zsh
+++ zsh/.zshrc.d/keybindings.zsh
@@ -1,5 +1,5 @@
 cd_fzf_exec() {
-  local dir=$(fzf_fasd)
+  local dir=$(fasd -l -d | grep -v '/\.' | fzf)
   if [[ -n $dir ]]; then
     cd $dir
   fi
@@ -13,9 +13,13 @@ find_in_mutt() {
   notmuch-mutt -r search "$*" && mutt -f ~/.cache/notmuch/mutt/results
 }
 
+switch_branch() {
+  git branch -a --no-merged origin/master | sed "s|remotes/origin/||" | cut -c2- | sort -u | fzf | xargs git checkout
+}
+
 bindkey -s '^P' "vim -c ':FZF!'\n"
 bindkey -s '^O' "vim -c ':GitFiles!?'\n"
 bindkey -s '^F' "find_in_vim "
 bindkey -s '^S' "find_in_mutt "
-bindkey -s '^K' 'cd_fzf_exec\n'
-bindkey -s '^G' 'git branch -a --no-merged origin/master | sed "s/remotes\\/origin\\///" | cut -c2- | sort -u | fzf | xargs git checkout\n'
+bindkey -s '^K' "cd_fzf_exec\n"
+bindkey -s '^G' "switch_branch\n"
