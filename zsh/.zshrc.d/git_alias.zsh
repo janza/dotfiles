06d8ee0f289f6d59a476d92ae5349945998fd9fe
Josip Janzic <me@josip.dev>
2020-04-14 15:30:04 +0200

    Add tig alias as replacement for actual tig

--- zsh/.zshrc.d/git_alias.zsh
+++ zsh/.zshrc.d/git_alias.zsh
@@ -14,6 +14,20 @@ log() {
   FZF-EOF"
 }
 
+tig() {
+  git log $1 --color=always --graph \
+    --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit \
+    | head -5000 \
+    | fzf --ansi +s --height 40 --reverse \
+    --preview 'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git show --color=always $1 ; }; f {}' \
+    --bind "j:down,k:up,alt-j:preview-down,alt-k:preview-up,ctrl-f:preview-page-down,ctrl-b:preview-page-up,q:abort,ctrl-m:execute:
+      (grep -o '[a-f0-9]\{7\}' | head -1 |
+      xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+      {}
+	FZF-EOF"
+}
+
+
 # Aliases
 alias g='git'
 compdef g=git
